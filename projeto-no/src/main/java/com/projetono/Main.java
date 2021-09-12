e40ccafb7894287b0f3d3809708d28c594d3b8d4
Braulio Carvalho <braulio.github@gmail.com>
2021-09-12 01:00:09 -0300

    Conceito de nó e encadeamento de nó

---
+++ projeto-no/src/main/java/com/projetono/Main.java
@@ -0,0 +1,27 @@
+package com.projetono;
+
+public class Main {
+
+    public static void main(String[] args){
+
+        No no1 = new No("No1");
+
+        No no2 = new No("No2");
+        //no 1 apontando para no2
+        no1.setProximono(no2);
+
+        No no3 = new No("No3");
+        //no 2 apontando para no3
+        no2.setProximono(no3);
+
+        No no4 = new No("No4");
+        //no 3 apontando para no 4
+        no3.setProximono(no4);
+
+        System.out.println(no1);
+        System.out.println(no1.getProximono());
+        System.out.println(no1.getProximono().getProximono().getProximono());
+        System.out.println(no1.getProximono().getProximono().getProximono().getProximono());
+
+    }
+}
